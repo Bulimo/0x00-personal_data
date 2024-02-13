3df9947b9ebba00bf86a9f4fe637870b4b620ae7
Eric Ubaga <ubaga91@gmail.com>
2024-02-13 08:49:45 +0300

    Corrected logic in the require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Module auth
-Implements the class Auth for basic authentication
+Implements the class Auth for authentication
 """
 from flask import request
 from typing import List, TypeVar
@@ -22,16 +22,17 @@ class Auth:
         """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        if path:
-            # Make sure all paths in excluded_paths end with a '/'
-            excluded_paths = [
-                p if p.endswith('*') else p + '/' for p in excluded_paths
-            ]
 
-            # Check if path matches any excluded paths
-            for excluded_path in excluded_paths:
-                if path.startswith(excluded_path.rstrip('*')):
-                    return False
+        # Make sure all paths in excluded_paths end with a '/'
+        excluded_paths = [
+            p if p.endswith('*') else p + '/' for p in excluded_paths
+        ]
+
+        # Check if path matches any excluded paths
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path.rstrip('*')) or \
+                    path in excluded_path:
+                return False
 
         return True
 
